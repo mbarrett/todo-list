9248e4d70eda578bbb0346af5990286d5b171d60
Mark Barrett <mbarrett@Marks-MacBook-Pro.local>
2013-11-13 15:17:06 +0000

    Trying to hook up the PG database in heroku

--- models.rb
+++ models.rb
@@ -1,5 +1,6 @@
 #DataMapper::Logger.new($stdout, :debug)
- DataMapper.setup(:default, 'postgres://localhost/todo-db')
+#DataMapper.setup(:default, 'postgres://localhost/todo-db')
+DataMapper.setup(:default, 'postgres://grerrjuorqwmxp:5KPVSqD63ZpSQ1fUmalMATbnnY@ec2-23-23-80-55.compute-1.amazonaws.com/d5ndetmmtlm6gv')
 
 class Task
 	include DataMapper::Resource
