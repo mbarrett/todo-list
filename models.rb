fd341aceb6c9d26ff2983b7a6522ea1d81bc3497
Mark Barrett <mbarrett@Marks-MacBook-Pro.local>
2013-11-15 15:34:09 +0000

    Changing the connection string AGAIN

--- models.rb
+++ models.rb
@@ -1,6 +1,6 @@
 #DataMapper::Logger.new($stdout, :debug)
 #DataMapper.setup(:default, 'postgres://localhost/todo-db')
-DataMapper.setup(:default, 'postgres://potkcyehhyvape:60qOcGkUUoiinfM2NO49gJoiNO@ec2-54-204-36-244.compute-1.amazonaws.com:5432/d9am60mfei9g48')
+DataMapper.setup(:default, 'postgres://ruxslksqjjfyvx:0vHBQzH6z4T2_AuoFUr96JnhB4@ec2-54-217-239-27.eu-west-1.compute.amazonaws.com:5432/d20jabf0992aa6')
 
 class Task
 	include DataMapper::Resource
